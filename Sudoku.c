41a25333029f988416d6d851ddb59df2e38c8c5a
Dilvonei <51748217+Dilvonei@users.noreply.github.com>
2019-06-20 16:14:12 -0300

    Update Sudoku.c

--- Sudoku.c
+++ Sudoku.c
@@ -1,90 +1,91 @@
-﻿#include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<conio.h>
 
-int bem_vindo();
+//int bem_vindo();
 int menu();
 int novo_jogo(int matriz[9][9]);
 int grid(int matriz[9][9]);
 int adicionar_jogada(int matriz[9][9]);
 int remover_jogada(int matriz[9][9]);
+int salvar_progresso(int matriz [9][9]);
+int carregar_jogo();
 int puzzleradom();
 
 int main(){
-  int matriz[9][9]={0};
   printf("\n");
-  bem_vindo();
+  //bem_vindo();
   menu();
-  puzzleradom();
+  return 0;
 }
 
-int menu(){
-  int opcao;
-  puts("\t\t  【M】【E】【N】【U】");
-  puts("");
-  puts("\t\t     1 -【Novo jogo】");
-  puts("\t\t     2 -【Carregar】");
-    puts("\t\t     3 -【Sair】");
-  printf("                     【Digite sua opcao】: ");
-    scanf("%d",&opcao);
-    system("clear");
-    switch(opcao){
+    int menu(){
+      int opcao;
+      puts("\t\t  【M】【E】【N】【U】");
+      puts("");
+      printf("Digite sua opção\n");
+      //puts("\t\t     1 -【Novo jogo】");
+      //puts("\t\t     2 -【Carregar】");
+      ///puts("\t\t     3 -【Sair】");
+      printf("[1-Novo Jogo]\n[2-Carregar]\n[3-Sair]\n");
+      scanf("%d",&opcao);
+      system("clear");
+      switch(opcao){
 
-      case 1:
-      printf("\t\t\tRegras do jogo\n");
-      printf("\tO objetivo do Sudoku é preencher todos os espaços vazios\n");
-      printf("\tcom numeros de 1 a 9. Porem o numero escolhido nao pode estar se\n");
-      printf("\trepetindo na mesma coluna, linha e bloco.\n");
-
-            break;
-      case 2: //carreagar_jogo();
-                break;
-      default:
-        return 0;
+        case 1:
+          printf("\t\t\tRegras do jogo\n");
+          printf("\tO objetivo do Sudoku é preencher todos os espaços vazios\n");
+          printf("\tcom numeros de 1 a 9. Porem o numero escolhido nao pode estar se\n");
+          printf("\trepetindo na mesma coluna, linha e bloco.\n");
+          puzzleradom();
+         break;
+        case 2: carregar_jogo();
+          break;
+        default:
+         return 0;
       }
     }
-
-      int grid(int matriz[9][9]){
+    int grid(int matriz[9][9]){
         int i, j;
         //printf("\033[39m");
         system("clear");
-        printf("\033[30m");
-        printf("\033[1m");
-        /*for(i = 0; i < 9; i++){
-          for(j = 0; j < 9; j++){
-            printf("%d ", matriz[i][j]);
+        for(i=0;i<9;i++){
+          for(j=0;j<9;j++){
+            printf("%d ",matriz[i][j]);
           }
           printf("\n");
         }
-      }*/
-        for (i = 0; i< 9; i++)
-        {
-          for (j = 0; j < 9; j++)
-          {
+        /*printf("\033[34m");
+        printf("\033[1m");
+        for (i = 0; i< 9; i++){
+          for (j = 0; j < 9; j++){
             if (j == 3 || j == 6)
               printf(" ");
-            if (i == 0 || i == 3 || i == 6)
-            {
+            if (i == 0 || i == 3 || i == 6){
               if (j == 0)
                printf(" _____  ");
               else
                printf("_____  ");
-             }
-           }
-
+            }
+          }
+          printf("\n");
+          for (j = 0; j < 9; j++){
+              if (j == 3 || j == 6)
+                printf(" ");
+              printf("|     |");
+            }
             printf("\n");
-            for (j = 0; j < 9; j++)
-            {
+            for (j = 0; j < 9; j++){
               if (j == 3 || j == 6)
                 printf(" ");
               if (matriz[i][j] == 0)
                 printf("|     |");
               else
-                printf("|  %d  |",matriz[i][j]);
+                printf("|  %i  |",matriz[i][j]);
             }
             printf("\n");
-            for (j = 0; j< 9; j++)
-            {
+            for (j = 0; j< 9; j++){
                 if (j == 3 || j == 6)
                   printf(" ");
                 printf("|_____|");
@@ -92,10 +93,10 @@ int menu(){
             if (i == 2 || i == 5)
               printf("\n");
         }
-        printf("\n");
-      }
+        printf("\n");*/
+    }
 
-    int adicionar_jogada(int matriz[9][9]){
+   int adicionar_jogada(int matriz[9][9]){
     int linha , coluna, jogada;
       for(;;){
         printf("Informe a linha desejada: ");
@@ -128,46 +129,64 @@ int menu(){
 
 
   int remover_jogada(int matriz[9][9]){
-  int linha , coluna;
+  int linha ,coluna;
         printf("Remova uma jogada:\n");
         printf("Informe a linha desejada: ");
         scanf("%d", &linha);
         printf("Informe a coluna desejada: ");
         scanf("%d",&coluna);
         printf("Vc removeu a jogada da Linha %d Coluna %d\n",linha, coluna);
-        matriz[linha - 1][coluna - 1]=0;
+        matriz[linha-1][coluna-1]=0;
         system("clear");
-        grid(matriz);
         novo_jogo(matriz);
+  }  
+  int salvar_progresso(int matriz[9][9]){
+      
+      FILE* save = fopen("saved.bin","wb");
+    if (save == NULL) {
+    printf("\nERRO SAVE COMRROPIDO.\n");
+    exit(1);
+    }
+    int i,j;
+    for(i=0;i<9;i++){
+      for(j=0;j<9;j++){
+        fprintf(save,"%d",matriz[i][j]);
+      }
+       fprintf(save,"\n",matriz[i][j]);
+    }
+    
+    printf("Jogo Salvo Com Sucesso.\n");
+    fclose(save);
+    novo_jogo(matriz);
+  }
+  int carregar_jogo(){
+    int matriz[9][9];
+    system("cls");
+    int i=0,j=0;
+    int recebe[9][9];
+    FILE *save=fopen("saved.txt","rb");
+    if (save == NULL) {
+      printf("\nERRO SAVE COMRROPIDO.\n");
+      exit(1);
+    }
+    printf("Jogo Carregado.\n");
+
+     for(i=0;i<9;i++){
+      for(j=0;j<9;j++){
+          fscanf(save,"%d ",&matriz[i][j]);
+      }
+    }
+    novo_jogo(matriz);
   }
 
-  int novo_jogo(int matriz [9][9]){
-    int op;
-  printf("Digite sua opcao: \n");
-  printf("1-Nova jogada.\n");
-  printf("2-Excluir jogada.\n");
-  printf("3-Salvar progresso.\n");
-  printf("4-Desistir.\n");
-  scanf("%d",&op);
-    grid(matriz);
-    switch (op) {
-      case 1: adicionar_jogada(matriz);
-              break;
-      case 2: remover_jogada(matriz);
-              break;
-      /*case 3: salvar_progresso(nome);
-              break;
-      case 4: return 0;*/
-    }
-  }
-  int bem_vindo(){
+  /*int bem_vindo(){
     printf("\033[36m") ;
     printf("\033[1m");
     printf("\033[05m");
-    puts("\t\t┏━━━┳┓╋┏┳━━━┳━━━┳┓┏━┳┓╋┏┓");
-    puts("\t\t┃┏━┓┃┃╋┃┣┓┏┓┃┏━┓┃┃┃┏┫┃╋┃┃");
-    puts("\t\t┃┗━━┫┃╋┃┃┃┃┃┃┃╋┃┃┗┛┛┃┃╋┃┃");
-    puts("\t\t┗━━┓┃┃╋┃┃┃┃┃┃┃╋┃┃┏┓┃┃┃╋┃┃");
+    puts("\t\t┏━━━┳┓1┏┳━━━┳━━━┳┓┏━┳┓3┏┓");
+    puts("\t\t┃┏━┓┃┃9┃┣┓┏┓┃┏━┓┃┃┃┏┫┃7┃┃");
+    puts("\t\t┃┗━━┫┃2┃┃┃┃┃┃┃0┃┃┗┛┛┃┃4┃┃");
+    puts("\t\t┗━━┓┃┃8┃┃┃┃┃┃┃5┃┃┏┓┃┃┃6┃┃");
     puts("\t\t┃┗━┛┃┗━┛┣┛┗┛┃┗━┛┃┃┃┗┫┗━┛┃");
     puts("\t\t┗━━━┻━━━┻━━━┻━━━┻┛┗━┻━━━┛");
     printf("\n");
@@ -179,9 +198,31 @@ int menu(){
     puts("\t  ┃╰━╯┃╰━━┫┃┃┃┃┣━━┻╮╭╯╭┫┣┫┃╱┃┃┣╯╰╯┃╰━╯┃");
     puts("\t  ╰━━━┻━━━┻╯╰╯╰╯╱╱╱╰╯╱╰━━┻╯╱╰━┻━━━┻━━━╯");
     puts("");
-}
+}*/
 
-int puzzleradom(){
+  int novo_jogo(int matriz [9][9]){
+    int op;
+  printf("Digite sua opcao: \n");
+  printf("1-Nova jogada.\n");
+  printf("2-Excluir jogada.\n");
+  printf("3-Salvar progresso.\n");
+  printf("4-Desistir.\n");
+  scanf("%d",&op);
+   
+    switch (op) {
+      case 1: grid(matriz); 
+              adicionar_jogada(matriz);
+              break;
+      case 2: grid(matriz); 
+              remover_jogada(matriz);
+              break;
+      //case 3: salvar_progresso(nome);
+              //break;
+      default: printf("VC E FRACO LHE FALTA SODOKU");
+      return 0;
+    }
+  }
+  int puzzleradom(){
       int radom;
       int matriz[9][9];
       int i,j;
@@ -198,11 +239,11 @@ int puzzleradom(){
 
       case 1:
        switch(radom){
-      case 1:select=fopen("facil_1.txt","rb");  break;
-      case 2:select=fopen("facil_2.txt","rb");  break;
-      case 3:select=fopen("facil_3.txt","rb");  break;
-      case 4:select=fopen("facil_4.txt","rb");  break;
-      case 5:select=fopen("facil_5.txt","rb");  break;
+      case 1:select=fopen("facil_1.txt","r");  break;
+      case 2:select=fopen("facil_2.txt","r");  break;
+      case 3:select=fopen("facil_3.txt","r");  break;
+      case 4:select=fopen("facil_4.txt","r");  break;
+      case 5:select=fopen("facil_5.txt","r");  break;
         default:
           return 0;
       }
